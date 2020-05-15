acdbab73ab88cda74702aae94ef749460711cb9d
efefurkankarakaya <efkdev@outlook.com>
2020-05-15 03:33:14 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# 403 Forbidden
-
-This is a test app to learn deploying React apps on Github Pages.
---
+++ precache-manifest.83d47392dd694c79cc68ef6310158541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c733de1e20831b39007eae15bc0ccaf",
+    "url": "/403-forbidden/index.html"
+  },
+  {
+    "revision": "f9dc6d0444776122347d",
+    "url": "/403-forbidden/static/css/main.d3113882.chunk.css"
+  },
+  {
+    "revision": "6c5aaec3ea7928efbb39",
+    "url": "/403-forbidden/static/js/2.a5c794e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/403-forbidden/static/js/2.a5c794e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9dc6d0444776122347d",
+    "url": "/403-forbidden/static/js/main.2ee6dc66.chunk.js"
+  },
+  {
+    "revision": "a93e9d3689c58df874c8",
+    "url": "/403-forbidden/static/js/runtime-main.b66ffbae.js"
+  }
+]);
