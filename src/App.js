746f2e552f991b83d33fe445cc32a5a2a5fff6a6
efefurkankarakaya <efkdev@outlook.com>
2020-05-15 04:08:02 +0300

    delete unnecessary code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App-header">
         <p align="center"><font size="20">403: Forbidden</font></p>
         <p className="App-logo" align="center"><font face="Courier new" size="20">**</font></p>
-        <br /><br /><br /><br /><br /><br /><br /><br /><br />
         <p align="center"><font size="1">github.com/efefurkankarakaya</font></p>
     </div>
   );
